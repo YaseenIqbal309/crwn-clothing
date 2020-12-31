870f383b38d327d03bb2b7d328c1f186f9e8640f
Yaseeniqbal309 <yaseeniqbal309@gmail.com>
2021-01-01 02:35:15 +0500

    Broker homepage into Smaller components and styling

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Directory from '../../components/directory/directory.component'
+import './homepage.styles.scss'
+
+const Homepage = () => (
+    <div className='homepage'>
+        <h1>Welcome to Homepage</h1>
+        <Directory />
+    </div>
+        
+);
+export default Homepage;
