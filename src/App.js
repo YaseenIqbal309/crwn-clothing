2b243f7401fd7a0a0571955047036839c6b587e2
Yaseeniqbal309 <yaseeniqbal309@gmail.com>
2021-01-09 18:57:11 +0500

    Updated Menu item withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
+import {Route,Switch} from 'react-router-dom'
 import Homepage from './pages/homepage/Homepage.component'
+
+
+const Hats=()=>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
+      <Switch>
+        <Route exact  path='/' component={Homepage}/>
+        <Route path='/shop/Hats' component={Hats} />
+        </Switch>
       
-      <Homepage />
+    
+      
+      
+
+      
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 import './menu-item.styles.scss'
-const MenuItem = ({title,imageUrl,size}) =>(
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) =>(
     <div 
-
+        onClick={()=>history.push(`${match.url}${linkUrl}`)}
      className={`${size} menu-item`}>
          <div className='background-image'
          style={{
@@ -16,4 +17,4 @@ const MenuItem = ({title,imageUrl,size}) =>(
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
